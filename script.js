1c01a575fc021c33782c0b43570029a32fc85a53
MustofaAli <127159575+MymaGit@users.noreply.github.com>
2023-08-07 15:18:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Function to handle smooth scrolling to sections
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        const offsetTop = section.offsetTop - 60;
+        window.scrollTo({
+            top: offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
 
